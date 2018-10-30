60ac02c0d0fb76b67f16bdbc9ac89fd085ce4ed1
Fernando Chorney <fernando.chorney@invenia.ca>
2018-10-30 13:28:33 -0500

    Added basic isequivalent functions
    - Added the base isequivalent stuff that we currently have.
    - Wrote a few tests to get 100% coverage
    - Should work with 0.6+

--- src/Equivalence.jl
+++ src/Equivalence.jl
@@ -1,5 +1,40 @@
 module Equivalence
 
-greet() = print("Hello World!")
+using DataFrames: AbstractDataFrame
+using Missings
+
+# Define a generic isequivalent function to compare all elements of an AbstractArray
+# using the equivalency eq functions below
+function isequivalent(eq::Function, x::AbstractArray, y::AbstractArray)
+    size(x) == size(y) || return false
+    for i in eachindex(x)
+        r = isequivalent(eq, x[i], y[i])
+        r === true || return r
+    end
+    return true
+end
+
+# Define a generic isequivalent function to compare all elements of an AbstractDataFrame
+# using the equivalency eq functions below
+function isequivalent(eq::Function, x::AbstractDataFrame, y::AbstractDataFrame)
+    names(x) == names(y) || return false
+
+    for col in names(x)
+        r = isequivalent(eq, x[col], y[col])
+        r === true || return r
+    end
+
+    return true
+end
+
+# Define our base helper functions
+isequivalent(eq::Function, x, y) = eq(x, y)
+isequivalent(x, y) = isequivalent(eq, x, y)
+
+# Define our base equivalency functions
+eq(x::Missing, y::Missing) = true
+eq(x::Missing, y) = false
+eq(x, y::Missing) = false
+eq(x, y) = x == y
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,61 @@
-using Equivalence
+using Equivalence: isequivalent
+using DataFrames
+using Missings
 using Compat.Test
 
-@testset "Equivalence.jl" begin
-    # Write your own tests here.
+@testset "AllTests" begin
+    # Integers
+    @test  isequivalent(1, 1)
+    @test !isequivalent(1, 2)
+    @test !isequivalent(2, 1)
+
+    # Floats
+    @test  isequivalent(1.0, 1.0)
+    @test !isequivalent(1.0, 2.0)
+    @test !isequivalent(2.0, 1.0)
+    @test !isequivalent(2.0, 2.0000000001)
+
+    # Missing
+    @test  isequivalent(missing, missing)
+    @test !isequivalent(missing, 1)
+    @test !isequivalent(1, missing)
+
+    # AbstractArray
+    a1 = [1, 2, 3, 4]
+    a2 = [1, 2, 3, 4]
+    a3 = [1, 2, 3, 5]
+    a4 = [1.0, 2.0, 3.0, 4.0]
+    a5 = [1.0, 2.0, 3.0, 4.0]
+    a6 = [1.0, 2.0, 3.0, 4.0000001]
+
+    @test  isequivalent(a1, a2)
+    @test !isequivalent(a2, a3)
+    @test !isequivalent(a3, a2)
+
+    @test  isequivalent(a4, a5)
+    @test !isequivalent(a5, a6)
+    @test !isequivalent(a6, a5)
+
+    # DataFrame
+    df1  = DataFrame(a=a1, b=a2)
+    df2  = DataFrame(a=a2, b=a3)
+    df3  = DataFrame(a=a3, b=a2)
+    df4  = DataFrame(a=a4, b=a5)
+    df5  = DataFrame(a=a5, b=a6)
+    df6  = DataFrame(a=a6, b=a5)
+    df7  = DataFrame(a=a1, b=a2)
+    df8  = DataFrame(a=a2, b=a1)
+    df9  = DataFrame(a=a4, b=a5)
+    df10 = DataFrame(a=a5, b=a4)
+
+    @test !isequivalent(df1, df2)
+    @test !isequivalent(df2, df3)
+    @test !isequivalent(df3, df2)
+
+    @test !isequivalent(df4, df5)
+    @test !isequivalent(df5, df6)
+    @test !isequivalent(df6, df5)
+
+    @test  isequivalent(df7, df8)
+    @test  isequivalent(df9, df10)
 end
